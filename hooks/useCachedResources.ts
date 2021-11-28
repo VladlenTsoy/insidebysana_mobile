745a43a205a90a931f44c8ee062932682b4053c0
vladlen <vladlengg@gmail.com>
2021-11-28 18:41:57 +0500

    add .prettierrc and ant icons

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -1,31 +1,33 @@
-import {FontAwesome} from '@expo/vector-icons';
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import {useState, useEffect} from 'react';
-import {useFonts, Montserrat_300Light} from '@expo-google-fonts/montserrat';
+import {FontAwesome} from "@expo/vector-icons"
+import * as Font from "expo-font"
+import * as SplashScreen from "expo-splash-screen"
+import {useState, useEffect} from "react"
+import {useFonts, Montserrat_300Light} from "@expo-google-fonts/montserrat"
 
 export default function useCachedResources() {
-    const [isLoadingComplete, setLoadingComplete] = useState(false);
-    const [fontsLoaded] = useFonts({Montserrat_300Light});
+    const [isLoadingComplete, setLoadingComplete] = useState(false)
+    const [fontsLoaded] = useFonts({Montserrat_300Light})
 
     // Загрузите любые ресурсы или данные, которые нам нужны, до рендеринга приложения.
     useEffect(() => {
         (async () => {
             try {
-                await SplashScreen.preventAutoHideAsync();
+                await SplashScreen.preventAutoHideAsync()
                 // Load fonts
                 await Font.loadAsync({
                     ...FontAwesome.font,
-                });
+                    "antoutline": require("@ant-design/icons-react-native/fonts/antoutline.ttf"),
+                    "antfill": require("@ant-design/icons-react-native/fonts/antfill.ttf")
+                })
             } catch (e) {
                 // Мы можем захотеть предоставить эту информацию об ошибке в службу отчетов об ошибках
-                console.warn(e);
+                console.warn(e)
             } finally {
-                setLoadingComplete(true);
-                await SplashScreen.hideAsync();
+                setLoadingComplete(true)
+                await SplashScreen.hideAsync()
             }
         })()
-    }, []);
+    }, [])
 
-    return isLoadingComplete && fontsLoaded;
+    return isLoadingComplete && fontsLoaded
 }
--- hooks/useColorScheme.ts
+++ hooks/useColorScheme.ts
@@ -1,8 +1,8 @@
-import {ColorSchemeName, useColorScheme as _useColorScheme} from 'react-native';
+import {ColorSchemeName, useColorScheme as _useColorScheme} from "react-native"
 
 // Значение useColorScheme всегда либо светлое, либо темное, но встроенное
 // тип предполагает, что он может быть нулевым. На практике этого не произойдет, поэтому
 // упрощает работу.
 export default function useColorScheme(): NonNullable<ColorSchemeName> {
-    return _useColorScheme() as NonNullable<ColorSchemeName>;
+    return _useColorScheme() as NonNullable<ColorSchemeName>
 }
